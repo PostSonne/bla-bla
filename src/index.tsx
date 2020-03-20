04e6e9d217978343b6fdd51bc9abd84b85938758
ABairamova <Aliya.Bairamova@firstlinesoftware.com>
2020-03-20 03:12:54 +0300

    making prototype

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,71 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+interface ITrackItem {
+    index: string,
+    image: string,
+    isLoved: boolean,
+    trackName: string,
+    artist: string,
+    listeningCounter: number,
+}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
+const AppStore = createStore(reducer);
+
+const getItems = (Tracks: ITrackItem[]) => {
+    const Items: ITrackItem[] = [];
+    Tracks.forEach(track => {
+        Items.push({
+            "index" : track["index"],
+            "image" : track["image"],
+            "isLoved" : track["isLoved"],
+            "trackName": track["trackName"],
+            "artist": track["artist"],
+            "listeningCounter": track["listeningCounter"]
+
+        })
+    });
+    return Items;
+};
+
+let Tracks = [
+    {
+        "index" : "1",
+        "image" : "",
+        "isLoved" : true,
+        "trackName": "Hey You",
+        "artist": "Ann",
+        "listeningCounter": 144
+    }
+];
+
+function setItems(Items: ITrackItem[]) {
+    AppStore.dispatch ({
+        type: "SET_STATE",
+        state: {
+            filters: [
+                {id: "shower", inuse: false },
+                {id: "pets", inuse: false },
+                {id: "flush", inuse: false },
+                {id: "water", inuse: false }
+            ],
+            markers: Items,
+            campgrounds: Items
+        }
+    })
+}
+
+setItems(getItems(Tracks));
+
+ReactDOM.render(
+    <Provider store={AppStore}>
+        <App />
+    </Provider>,
+    document.getElementById("root")
+);
+
 serviceWorker.unregister();
