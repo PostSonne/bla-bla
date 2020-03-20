af4418a997747f97e7139b01982120c6cf6f330e
ABairamova <Aliya.Bairamova@firstlinesoftware.com>
2020-03-20 04:10:14 +0300

    adding types

--- src/Filters/ItemsFilter.tsx
+++ src/Filters/ItemsFilter.tsx
@@ -5,11 +5,7 @@ const ItemsFilter: FC = () => {
         <div className="col-sm-2">
             <input type="checkbox"
                    className="toggle"
-                   id={id}
-                   defaultChecked={this.props.inuse}
-                   onClick={() => this.props.changeFilter(this.props.id)}
             />
-            <label ref="text">{this.props.id}</label>
         </div>
     )
 };
